d254c822546e0503feab58d730cac1637ab17465
super <joel.jimenez.lorente@gmail.com>
2020-02-03 17:59:44 +0100

    register

--- loginnode/index.js
+++ loginnode/index.js
@@ -1,28 +1,59 @@
-
 var express = require('express');
 var app = express();
-app.set('view engine', 'ejs');
-var body_parser = require('body-parser');
-app.use(body_parser.urlencoded({extended:true}));
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
 
-var users = {
-	"paco":"noni",
-	"oscar":"noni",
-};
+app.set('view engine', 'ejs');
+
+var LocalStorage = require('node-localstorage').LocalStorage,
+localStorage = new LocalStorage('./scratch');
+
 
 app.get('/login', function (req, res) {
-  res.render('loginform');
+  res.render('loginForm');
+  
 });
 
+
 app.post('/login', function (req, res) {
-	var user = "";
-	var inputUser = req.body.username;
-	if (inputUser in users) {
-		if (req.body.password ==users[inputUser]) {
-			user = inputUser;
-		}
-	}
-	res.render('loginformPost', {user:user});
+  var inputUser = req.body.username;
+  var inputPassword = req.body.password;
+  if (localStorage.getItem(inputUser)) {
+    if (inputPassword == localStorage.getItem(inputUser)) {
+      user = inputUser;
+    }else{
+      user= "";
+    }
+  }else{
+    user=""
+  }
+  res.render('loginFormPost.ejs', {user:user})
+});
+
+app.get('/register', function (req, res) {
+  res.render('registerForm');
+  
+});
+app.post('/register', function (req, res) {
+  var name = req.body.username;
+  var pass = req.body.password;
+  var pass2 = req.body.passwordConf;
+  
+  
+  if (pass==pass2) {
+    passwordStatus="OK";
+  }else{
+    passwordStatus="KO";
+  }
+  if (localStorage.getItem(name)) {
+    usernameStatus= "KO";
+  }else{
+    usernameStatus= "OK";
+  }
+  if (usernameStatus=="OK" && passwordStatus=="OK") {
+    localStorage.setItem(name, pass)
+  }
+  res.render('registerFormPost.ejs', {passStatus:passwordStatus,userStatus:usernameStatus})
 });
 
 app.listen(3000, function () {
