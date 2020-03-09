19a3d24b7d1d8796a0ab0352f3dc902f883ca80f
super <joel.jimenez.lorente@gmail.com>
2020-03-09 17:55:43 +0100

    add business show all business

--- restaurants/app.js
+++ restaurants/app.js
@@ -5,19 +5,20 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 // per renderitzar les plantilles (render)
-app.set('view engine','ejs');
+
 require('dotenv').config();
-  
+app.set('view engine','ejs');  
 var mongoClient;
 // connexió a <span class="search_hit">mongo</span> i start app
-var mongo = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
+
 
 // consts
 const PORT = process.env.PORT || 5000
 const user = encodeURIComponent( process.env.DBUSER );
 const pass = encodeURIComponent( process.env.DBPASS );
 var dbConStr = "mongodb+srv://"+user+":"+pass+"@cluster0-wugkh.mongodb.net";
-mongo.connect( dbConStr, function( err, _client ) {
+MongoClient.connect( dbConStr, function( err, _client ) {
     // si no ens podem connectar, sortim
     if( err ) throw err;
     mongoClient = _client;
@@ -28,27 +29,38 @@ mongo.connect( dbConStr, function( err, _client ) {
 });
 
 
-
+app.get('', function (req, res) {
+    res.render('index')
+});
 
 app.get('/restaurant', function (req, res) {
     res.render('restaurant');
 });
 
-
-
-
-app.post('/restaurant', function (req, res) {
-    MongoClient.connect(url, function(err, db) {
+app.get('/categorias', function (req, res) {
+    MongoClient.connect(dbConStr, function(err, db) {
       if (err) throw err;
       var dbo = db.db("restaurant");
-      var myobj = { name: req.body.username, categoria: req.body.categoria, address: req.body.address};
+      dbo.collection("restaurants").find().toArray(function(err, results){
+        res.render("restaurantPost", { result: results });
+        db.close();
+        
+    });
+
+});
+    });
+app.post('/restaurant', function (req, res) {
+    MongoClient.connect(dbConStr, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("restaurant");
+      var myobj = { nom: req.body.username, poblacio: req.body.poblacio , direccio: req.body.direccio , cp: req.body.cp , telefon: req.body.telefon };
       dbo.collection("restaurants").insertOne(myobj, function(err, res) {
         if (err) throw err;
         console.log("1 document inserted");
         db.close();
       });
     }); 
-    res.render('restaurantPost')
+    res.render('restaurant',{ result: "Restaurante añadido" });
 });
 
 
